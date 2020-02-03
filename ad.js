741ab560e87db2e4a264b6123f3430f918f2496a
afdaljawab <60604643+afdaljawab@users.noreply.github.com>
2020-02-03 13:04:28 +0200

    Delete ad.js

--- ad.js
+++
@@ -1,7 +0,0 @@
-
-async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"
-<ins class="adsbygoogle"
-     style="display:inline-block;width:970px;height:250px"
-     data-ad-client="ca-pub-5526604290469240"
-     data-ad-slot="1330653309"></ins>
-(adsbygoogle = window.adsbygoogle || []).push({});
